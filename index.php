d46a36252aeddeeb156a863ba9458aa3d5062d04
Yappiyapi <yynissy.718@gmail.com>
2020-03-04 10:42:30 +0900

    乗車降車がうまく表示されない

--- index.php
+++ index.php
@@ -42,7 +42,33 @@ class Car {
   }
 }
 
+class Taxi extends  Car {
+  private $passenger;//現乗車人数を管理
 
+  public function getPassenger($passenger)
+  {
+    return $this->passenger;
+  }
+
+  public function getPickup($pickup) //乗車
+  {
+    return $this->pickUp = $passenger;
+  }
+
+  public function getLower($lower)//降車
+  {
+    if ($this->$passenger - $lower > 0) {
+      echo $lower . '人降車しました。';
+    }else{
+      echo '降車人数に誤りがあります。';
+    }
+  }
+
+  public function infomation()
+  {
+    echo "車の車種:" . $this->getName() . "、車体番号:" . $this->getNumber() . "、カラー:" . $this->getColor() . "、乗車人数は" . $this->getPassenger() . "人です。";
+  }
+}
 
 // 以下を実行した時にエラーがでないようにして下さい。
 // Carクラスのインスタンスを生成
@@ -50,38 +76,24 @@ class Car {
 $car1 = new Car('ランクル', 100, 'black');
 
 
-// $car1->selfIntrodaction();
-// 車名(車種)を取得
-echo $car1->getName();
-echo '<br>';
-
-// 車体番号を取得
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
+// 車の情報を表示
 echo $car1->infomation();
 echo '<br>';
+echo '<hr>';
 
-// 車名(車種)を更新
-$car1->setName('アルファード');
-echo $car1->getName();
+// Taxiクラスのインスタンスを生成
+$taxi1 = new Taxi('クラウンのタクシー', 222, 'black');
+
+// 3人乗車
+$taxi1->pickUp(3);
+
+// タクシーの情報表示を表示
+echo $taxi1->infomation();
 echo '<br>';
 
-// 車体番号を取得
-$car1->setNumber(200);
-echo $car1->getNumber();
+// 2人降車
+$taxi1->lower(2);
 echo '<br>';
 
-// カラーを取得
-$car1->setColor('red');
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->infomation();
-echo '<br>';
+// 一人降車
+$taxi1->lower(2);
